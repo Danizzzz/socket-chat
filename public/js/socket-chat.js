35129d631356e56e423eaf1e916c88a7a5964ff1
Daniel <gonza.daniel7@gmail.com>
2019-07-15 23:57:50 +0200

    Listo para produccion

--- public/js/socket-chat.js
+++ public/js/socket-chat.js
@@ -12,19 +12,25 @@ var usuario = {
     sala: params.get('sala')
 };
 
+
+
 socket.on('connect', function() {
     console.log('Conectado al servidor');
+
     socket.emit('entrarChat', usuario, function(resp) {
         // console.log('Usuarios conectados', resp);
-        console.log('Usuarios conectados', resp);
+        renderizarUsuarios(resp);
     });
 
 });
 
 // escuchar
 socket.on('disconnect', function() {
+
     console.log('Perdimos conexión con el servidor');
+
 });
+
 
 // Enviar información
 // socket.emit('crearMensaje', {
@@ -37,16 +43,19 @@ socket.on('disconnect', function() {
 // Escuchar información
 socket.on('crearMensaje', function(mensaje) {
     // console.log('Servidor:', mensaje);
-    console.log('Servidor: ', mensaje)
+    renderizarMensajes(mensaje, false);
+    scrollBottom();
 });
 
 // Escuchar cambios de usuarios
 // cuando un usuario entra o sale del chat
 socket.on('listaPersona', function(personas) {
-    console.log(personas);
+    renderizarUsuarios(personas);
 });
 
 // Mensajes privados
 socket.on('mensajePrivado', function(mensaje) {
+
     console.log('Mensaje Privado:', mensaje);
+
 });
